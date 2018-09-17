bf72bc78512db20bce1f85282ddbcc0e426f8c68
mily77 <773258709@qq.com>
2018-09-17 16:00:40 +0800

    Initial Commit

---
+++ HSxLivePlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HSxLivePlayerDemo
+//
+//  Created by emily on 2018/9/17.
+//  Copyright © 2018年 emily. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
