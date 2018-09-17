53245908b5f0924703a0aecfe4691c08f6fc5f0f
mily77 <773258709@qq.com>
2018-09-17 16:19:43 +0800

    init

---
+++ HSxLivePlayerDemo/Classes/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  HSxLivePlayerDemo
+//
+//  Created by emily on 2018/9/17.
+//  Copyright © 2018年 emily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ HSxLivePlayerDemo/Classes/BaseViewController.m
@@ -0,0 +1,22 @@
+//
+//  BaseViewController.m
+//  HSxLivePlayerDemo
+//
+//  Created by emily on 2018/9/17.
+//  Copyright © 2018年 emily. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
